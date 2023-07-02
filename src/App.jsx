26e9254928c0e74e08bdc053698c4bd92b8c66c3
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-02 18:39:06 +0700

    Add key props to FormSplitBill component

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,7 @@ const App = () => {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
